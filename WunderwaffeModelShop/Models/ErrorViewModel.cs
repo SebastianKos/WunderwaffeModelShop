613598b1079e0a43f44d01fb22a3db5659f08797
Sebastian <Sebastian@DESKTOP-J4VD9DP>
2024-12-23 15:23:46 +0100

    Dodaj pliki projektów.

---
+++ WunderwaffeModelShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WunderwaffeModelShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
